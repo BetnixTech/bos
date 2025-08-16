6e0abc0d0fefb8a763d564f75d403d23fc02eb51
Betnix <kumenitbemnet@gmail.com>
2025-08-16 12:38:20 -0400

    Update task.h

--- task.h
+++ task.h
@@ -6,10 +6,14 @@ typedef struct {
     int active;
     int pid;
     char name[32];
+    uint32_t esp; // stack pointer
+    uint32_t ebp; // base pointer
 } Task;
 
 void task_add(void (*entry)(), const char* name);
+void task_init();
 void task_switch();
 void task_run();
+void task_yield(); // manual yield if needed
 
 #endif
