b37ed6a88c9ca1abc54bb48c553b4b567009f75d
Betnix <kumenitbemnet@gmail.com>
2025-08-16 12:17:33 -0400

    Update ports.c

--- src/ports.c
+++ src/ports.c
@@ -1,11 +1,11 @@
 #include "ports.h"
 
 unsigned char inb(unsigned short port) {
-    unsigned char result;
-    __asm__ volatile ("inb %1, %0" : "=a"(result) : "dN"(port));
-    return result;
+    unsigned char ret;
+    __asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
 }
 
 void outb(unsigned short port, unsigned char data) {
-    __asm__ volatile ("outb %0, %1" : : "a"(data), "dN"(port));
+    __asm__ volatile ("outb %0, %1" : : "a"(data), "Nd"(port));
 }
