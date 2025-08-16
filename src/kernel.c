098c20dd338ce0ce70f5116ae0aa960638ef4602
Betnix <kumenitbemnet@gmail.com>
2025-08-16 10:52:59 -0400

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,16 +1,8 @@
 extern void terminal_clear(void);
 extern void terminal_write(const char* str);
-extern char keyboard_read(void);
 
 void kmain(void) {
     terminal_clear();
-    terminal_write("Betnix OS Booted!\nType something:\n");
-
-    while (1) {
-        char c = keyboard_read();
-        if (c) {
-            char str[2] = {c, 0};
-            terminal_write(str);
-        }
-    }
+    terminal_write("Betnix OS Booted!\n");
+    terminal_write("Welcome to your new OS.\n");
 }
