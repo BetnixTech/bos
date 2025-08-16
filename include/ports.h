793cbd43b787c89374d2f1f5a202ddb296b37bcb
Betnix <kumenitbemnet@gmail.com>
2025-08-16 11:57:51 -0400

    Create ports.h

---
+++ include/ports.h
@@ -0,0 +1,10 @@
+#ifndef PORTS_H
+#define PORTS_H
+#include <stdint.h>
+
+#define KEYBOARD_CTRL 0x64
+
+unsigned char inb(unsigned short port);
+void outb(unsigned short port, unsigned char data);
+
+#endif
