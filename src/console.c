e9b6fb0250ba8157681d6f84cb6e526f21282a90
Betnix <kumenitbemnet@gmail.com>
2025-08-16 12:00:09 -0400

    Update console.c

--- src/console.c
+++ src/console.c
@@ -1,10 +1,21 @@
 #include "console.h"
-#include <stdio.h> // For now, print to host terminal
+#include <stdint.h>
 
-void terminal_write(const char* str) {
-    printf("%s", str);
+uint16_t* video_memory = (uint16_t*)0xB8000;
+int terminal_row = 0;
+int terminal_col = 0;
+
+void terminal_clear() {
+    for(int y=0;y<25;y++)
+        for(int x=0;x<80;x++)
+            video_memory[y*80+x] = (0x0F << 8) | ' ';
+    terminal_row = terminal_col = 0;
 }
 
-void terminal_clear(void) {
-    printf("\033[2J\033[H"); // Clear screen ANSI escape
+void terminal_write(const char* str) {
+    while(*str) {
+        if(*str=='\n') { terminal_col=0; terminal_row++; }
+        else video_memory[terminal_row*80+terminal_col++] = (0x0F<<8)|*str;
+        str++;
+    }
 }
