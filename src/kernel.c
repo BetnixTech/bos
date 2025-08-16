f8bbf9e6909e45499035b9a434a62412c41c2df3
Betnix <kumenitbemnet@gmail.com>
2025-08-16 10:43:03 -0400

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,8 @@
-// Simple kernel: writes "Betnix OS" to VGA text mode
+extern void terminal_clear(void);
+extern void terminal_write(const char* str);
+
 void kmain(void) {
-    volatile unsigned short *video = (unsigned short*)0xB8000;
-    const char *msg = "Betnix OS - bootable demo";
-    for (int i = 0; msg[i] != 0; ++i) {
-        video[i] = (unsigned short)msg[i] | (0x0F << 8);
-    }
-    while (1) { } // hang
+    terminal_clear();
+    terminal_write("Betnix OS Booted!\n");
+    terminal_write("Welcome to your new OS.\n");
 }
