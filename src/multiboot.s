0cdb21edabb043e53d640d1836bbe87f64945ad3
Betnix <kumenitbemnet@gmail.com>
2025-08-17 12:16:16 -0400

    Create multiboot.s

---
+++ src/multiboot.s
@@ -0,0 +1,20 @@
+/* Multiboot header for GRUB (MB1) */
+.set ALIGN,    1<<0
+.set MEMINFO,  1<<1
+.set FLAGS,    ALIGN | MEMINFO
+.set MAGIC,    0x1BADB002
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+.section .text
+.global _start
+_start:
+    call kmain
+.hang:
+    hlt
+    jmp .hang
